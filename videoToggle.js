3c832099283183898ddfed7c59ed3685d0d17856
Rosendo99er <stephan.vila@cream-collection.de>
2025-01-23 09:50:12 +0100

    Update videoToggle.js

--- videoToggle.js
+++ videoToggle.js
@@ -1,20 +1,14 @@
 export function toggleVideoContainer(consentGiven) {
     const videoContainer = document.getElementById('video-container');
     const videoContainerBox = document.getElementById('video-container-box');
-    const chatbotMessages = document.getElementById('chatbot-messages');
-    const chatbotMessagesGPT = document.getElementById('chatbot-messages-gpt');
 
     if (consentGiven) {
       videoContainer.style.display = 'none';
       videoContainerBox.style.display = 'none';
-      chatbotMessages.style.display = 'block';
-      chatbotMessagesGPT.style.display = 'block';
       
     } else {
       videoContainer.style.display = 'block';
       videoContainerBox.style.display = 'block';
-      chatbotMessages.style.display = 'none';
-      chatbotMessagesGPT.style.display = 'none';
       
     }
   }
