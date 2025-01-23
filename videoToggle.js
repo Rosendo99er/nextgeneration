559c2489960a572e508ab222f2d794b2b558da6c
Rosendo99er <stephan.vila@cream-collection.de>
2025-01-23 09:59:59 +0100

    Update videoToggle.js

--- videoToggle.js
+++ videoToggle.js
@@ -1,14 +1,11 @@
 export function toggleVideoContainer(consentGiven) {
     const videoContainer = document.getElementById('video-container');
-    const videoContainerBox = document.getElementById('video-container-box');
-
+    
     if (consentGiven) {
       videoContainer.style.display = 'none';
-      videoContainerBox.style.display = 'none';
-      
+          
     } else {
       videoContainer.style.display = 'block';
-      videoContainerBox.style.display = 'block';
-      
+         
     }
   }
